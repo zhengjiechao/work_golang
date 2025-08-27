9dfeea0a6876c1611208d5d1733c4e6113217fe0
zhengjiechao <150@qq.com>
2025-08-27 21:57:42 +0800

    任务2

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 func main() {
+
+	// 任务一
+
 	// // 1.只出现一次的数字
 	// slice := []int{1, 1, 2, 5, 2, 4, 4}
 	// result := task1.SingleNumber(slice)
@@ -37,5 +40,65 @@ func main() {
 	// intArr := []int{5, 9, 8, 9}
 	// result := task1.TwoSum(intArr, 14)
 	// fmt.Println(result)
+
+	// 任务二
+
+	// // 1.1
+	// num := 100
+	// task2.Pointer1(&num)
+	// fmt.Println(num)
+
+	// // 1.2
+	// arr := []int{1, 2, 3}
+	// task2.Pointer2(&arr)
+	// fmt.Println(arr)
+
+	// // 2.1
+	// task2.Goroutine1()
+
+	// 2.2
+	// arr := []func(){
+	// 	func() {
+	// 		time.Sleep(time.Second * 1)
+	// 	},
+	// 	func() {
+	// 		time.Sleep(time.Second * 2)
+	// 	},
+	// 	func() {
+	// 		time.Sleep(time.Millisecond * 500)
+	// 	},
+	// }
+	// task2.Goroutine2(arr)
+
+	// // 3.1
+	// r := task2.Rectangle{}
+	// r.Area()
+	// r.Perimeter()
+
+	// c := task2.Circle{}
+	// c.Area()
+	// c.Perimeter()
+
+	// // 3.2
+	// e := task2.Employee{
+	// 	EmployeeID: 1,
+	// 	Person: task2.Person{
+	// 		Name: "zhagnsan",
+	// 		Age:  55,
+	// 	},
+	// }
+	// e.PrintInfo()
+
+	// // 4.1
+	// task2.Channel1()
+
+	// // 4.2
+	// task2.Channel2()
+
+	// // 5.1
+	// task2.Lock1()
+
+	// // 5.2
+	// task2.Lock2()
 
 }
---
+++ task2/part2.go
@@ -0,0 +1,192 @@
+package task2
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+var lock sync.Mutex
+
+// 指针
+// 1.1
+func Pointer1(num *int) {
+	*num += 10
+}
+
+// 1.2
+func Pointer2(arr *[]int) {
+	for i := 0; i < len(*arr); i++ {
+		(*arr)[i] *= 2
+	}
+}
+
+// 2.Goroutine
+// 2.1
+func Goroutine1() {
+	wg.Add(1)
+	go func() {
+		for i := 1; i <= 10; i += 2 {
+			fmt.Println(i, "--协程1")
+		}
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		for i := 2; i <= 10; i += 2 {
+			fmt.Println(i, "==协程2")
+		}
+	}()
+	wg.Done()
+	wg.Wait()
+}
+
+// 2.2
+func Goroutine2(arr []func()) {
+	wg.Add(len(arr))
+	for _, f := range arr {
+		go func() {
+			defer wg.Done()
+			startUnix := time.Now().UnixMilli()
+			f()
+			fmt.Printf("耗时：%v 毫秒\n", time.Now().UnixMilli()-startUnix)
+		}()
+	}
+	wg.Wait()
+}
+
+// 3.面向对象
+// 3.1
+type Shape interface {
+	Area()
+	Perimeter()
+}
+
+type Rectangle struct {
+}
+
+func (r Rectangle) Area() {
+	fmt.Println("我是Rectangle的Area")
+}
+
+func (r Rectangle) Perimeter() {
+	fmt.Println("我是Rectangle的Perimeter")
+}
+
+type Circle struct {
+}
+
+func (r Circle) Area() {
+	fmt.Println("我是Circle的Area")
+}
+
+func (r Circle) Perimeter() {
+	fmt.Println("我是Circle的Perimeter")
+}
+
+// 3.2
+type Person struct {
+	Name string
+	Age  int
+}
+
+type Employee struct {
+	EmployeeID int
+	Person
+}
+
+func (e Employee) PrintInfo() {
+	fmt.Println("EmployeeID:", e.EmployeeID)
+	fmt.Println("Name:", e.Name)
+	fmt.Println("Age:", e.Age)
+}
+
+// 4.Channel
+// 4.1
+func Channel1() {
+	ch := make(chan int, 5)
+
+	wg.Add(1)
+	go func() {
+		for i := 1; i <= 10; i++ {
+			ch <- i
+			fmt.Println("发送一个：", i)
+		}
+		close(ch)
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		for v := range ch {
+			fmt.Println("接收到一个：", v)
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+// 4.2
+func Channel2() {
+	ch := make(chan int, 10)
+
+	wg.Add(1)
+	go func() {
+		for i := 1; i <= 100; i++ {
+			ch <- i
+			fmt.Println("生产者发送一个：", i)
+		}
+		close(ch)
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		for v := range ch {
+			fmt.Println("消费者接收到一个：", v)
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+// 5.锁机制
+// 5.1
+func Lock1() {
+	num := 0
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			for j := 0; j < 1000; j++ {
+				lock.Lock()
+				num += 1
+				lock.Unlock()
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(num)
+}
+
+// 5.2
+func Lock2() {
+	var num int64 = 0
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			for j := 0; j < 1000; j++ {
+				atomic.AddInt64(&num, 1)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(num)
+}
